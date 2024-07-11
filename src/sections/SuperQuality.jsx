799dc773d94a04a9f77a47d87b49805fe81340a4
WGreenwichChaca <walter.greenwich@gmail.com>
2024-07-10 21:30:13 -0500

    Agregando la seccion SpecialOffer

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
-import { shoe8 } from "../assets/images"
+import { shoe8 } from "../assets/images";
+import { arrowRight } from "../assets/icons"
 
 const SuperQuality = () => {
   return (
@@ -14,24 +15,26 @@ const SuperQuality = () => {
           <span className="text-coral-red">Quality </span> Shoes
         </h2>
         <p className="mt-4 lg:max-x-lg info-text">
-          Ensuring premium comfort and style, our meticulously  crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
         </p>
         <p className="mt-6 lg:max-w-lg info-text">
           Our dedication to detail and excellence ensures your satisfaction
         </p>
-        <div>
-        <Button label="View details" />
+        <div className="mt-11">
+          <Button label="Shop now" iconURL={arrowRight} />
         </div>
       </div>
 
       <div className="flex-1 flex justify-center items-center">
-        <img 
-        src={shoe8} 
-        alt="shoe8"
-        width={570}
-        height={522}
-        className="object-contain"
-         />
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
       </div>
     </section>
   );
